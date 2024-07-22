3eb38e6bd37b046f0607441070f15ca9aaa9ec38
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-07-22 18:03:35 +0100

    new update

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ userSchema.pre('save', async function (next) {
     const user = this;
     if (!user.isModified('password')) return next();
 
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(user.password, salt);
     user.password = hash;
     next();
