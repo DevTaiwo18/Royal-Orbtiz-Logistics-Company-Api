199ea58412563d601b019492d33e23f2536c046b
DevTaiwo18 <adeyemitaiwo24434@gmail.com>
2024-07-22 18:21:01 +0100

    new update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,53 +1,51 @@
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
 
 exports.login = async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  try {
-    console.log('Login request received with:', { username, password });
+    try {
+        console.log('Login request received with:', { username, password });
 
-    const user = await User.findOne({ username });
-    if (!user) {
-      console.log('User not found');
-      return res.status(404).json({ message: 'User not found' });
+        const user = await User.findOne({ username });
+        if (!user) {
+            console.log('User not found');
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            console.log('Invalid credentials');
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        console.log('Login successful');
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        console.error('Error in login:', error);
+        res.status(500).json({ message: 'Server error' });
     }
-
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      console.log('Invalid credentials');
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    console.log('Login successful');
-    res.status(200).json({ message: 'Login successful' });
-  } catch (error) {
-    console.error('Error in login:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
 };
 
-
 exports.changePassword = async (req, res) => {
-  const { username, currentPassword, newPassword } = req.body;
+    const { username, currentPassword, newPassword } = req.body;
 
-  try {
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isMatch = await user.comparePassword(currentPassword);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Current password is incorrect' });
+        }
+
+        user.password = newPassword;
+        await user.save();
+
+        res.status(200).json({ message: 'Password updated successfully' });
+    } catch (error) {
+        console.error('Error in changePassword:', error);
+        res.status(500).json({ message: 'Server error' });
     }
-
-    const isMatch = await user.comparePassword(currentPassword);
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Current password is incorrect' });
-    }
-
-    user.password = await bcrypt.hash(newPassword, 12);
-    await user.save();
-
-    res.status(200).json({ message: 'Password updated successfully' });
-  } catch (error) {
-    console.error('Error in changePassword:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
 };
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -19,22 +18,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
-};
-
-
-userSchema.pre('save', async function (next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-
-    const salt = await bcrypt.genSalt(12);
-    const hash = await bcrypt.hash(user.password, salt);
-    user.password = hash;
-    next();
-});
-
-userSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+    return candidatePassword === this.password;
 };
 
 module.exports = mongoose.model('User', userSchema);
